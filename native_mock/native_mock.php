620f2d35dac3a919fcd545f50ba27436923ee009
Jacek Kuzemczak <jacek@kuzemczak.co.uk>
2016-12-12 22:46:30 +0000

    Added abillity to redefine existing functions

--- native_mock/native_mock.php
+++ native_mock/native_mock.php
@@ -6,6 +6,65 @@ namespace betterphp\native_mock;
 
 trait native_mock {
 
+    private $redefined_functions;
 
+    /**
+     * Should be called from TestCase::setUp()
+     *
+     * @return void
+     */
+    protected function nativeMockSetUp() {
+        if (!extension_loaded('uopz')) {
+            $this->markTestSkipped('The uopz extension is required for this test https://github.com/krakjoe/uopz');
+        }
+
+        $this->redefined_functions = [];
+    }
+
+    /**
+     * Should be called from TestCase::tearDown()
+     *
+     * @return void
+     */
+    protected function nativeMockTearDown() {
+        foreach ($this->redefined_functions as $function_name) {
+            $this->resetFunction($function_name);
+        }
+    }
+
+    /**
+     * Redefined a built-in or user defined function
+     *
+     * It's a good idea to make the new function accept either no parameters
+     * or the same ones as the original.
+     *
+     * @param string $function_name The name of the function
+     * @param \Closure $replacement The new function
+     *
+     * @return void
+     */
+    protected function redefineFunction(string $function_name, \Closure $replacement) {
+        uopz_set_return($function_name, $replacement, true);
+
+        $this->redefined_functions[] = $function_name;
+    }
+
+    /**
+     * Resets a function to it's original value
+     *
+     * @param string $function_name The name of the function
+     *
+     * @return void
+     */
+    protected function resetFunction(string $function_name) {
+        uopz_unset_return($function_name);
+
+        $this->redefined_functions = array_filter(
+            $this->redefined_functions,
+            function ($redefined) use ($function_name) {
+                return $redefined !== $function_name;
+            }
+        );
+    }
 
 }
--- tests/native_mock/NativeMockTest.php
+++ tests/native_mock/NativeMockTest.php
@@ -10,6 +10,57 @@ class NativeMockTest extends TestCase {
 
     use native_mock;
 
+    public function setUp() {
+        $this->nativeMockSetUp();
+    }
 
+    public function tearDown() {
+        $this->nativeMockTearDown();
+    }
+
+    public function testRedefineNativeFunction() {
+        $expected_value = 400;
+
+        $this->redefineFunction('strpos', function () use ($expected_value) {
+            return $expected_value;
+        });
+
+        $this->assertSame($expected_value, strpos('not used', 'not used'));
+    }
+
+    public function testRedefinedFunctionsReset() {
+        $test_string = 'such test, very string, wow.';
+
+        $this->assertSame(11, strpos($test_string, 'very'));
+    }
+
+    public function testRedefineUserfunction() {
+        $test_string = 'such test, very string, wow.';
+
+        // Inner function are normally not a good idea but we need one to test with here
+        function example_user_function() { //@codingStandardsIgnoreLine
+            return 'nothing like the above';
+        }
+
+        $this->redefineFunction('example_user_function', function () use ($test_string) {
+            return $test_string;
+        });
+
+        $this->assertSame($test_string, example_user_function());
+    }
+
+    public function testResetfunction() {
+        $expected_value = 'crikey this isn\'t a file';
+
+        $this->redefineFunction('file_get_contents', function () use ($expected_value) {
+            return $expected_value;
+        });
+
+        $this->assertSame($expected_value, file_get_contents(__FILE__));
+
+        $this->resetFunction('file_get_contents');
+
+        $this->assertNotSame($expected_value, file_get_contents(__FILE__));
+    }
 
 }
